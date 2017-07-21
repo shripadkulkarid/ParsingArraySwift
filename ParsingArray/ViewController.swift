12207c62060795bb90a3d2539bd81ce1907ea14e
Apple <MD104HNA@apples-macbook-pros.local>
2017-07-21 18:23:35 +0530

    Swift 3 Parsing Array

--- ParsingArray/ViewController.swift
+++ ParsingArray/ViewController.swift
@@ -8,11 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    
+    
+    var names:[String] = []
+    var contacts:[String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://api.androidhive.info/contacts/")
+        
+        do {
+           let allContactsData = try Data(contentsOf: url!)
+            
+            let allcontacs = try JSONSerialization.jsonObject(with: allContactsData, options: .allowFragments) as! [String :AnyObject]
+            
+            if let arrJSON = allcontacs["contacts"]  {
+                
+                for index in 0...arrJSON.count-1 {
+                    let aObject = arrJSON[index] as! [String : AnyObject]
+                    
+                   names.append(aObject["name"] as! String)
+                    contacts.append(aObject["email"] as! String)
+                    
+                    
+                    
+                }
+                
+                
+                
+                
+            }
+            
+            print(names)
+            print(contacts)
+            
+            self.tableview.reloadData()
+            
+            
+            
+            
+            
+            
+        } catch{}
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +65,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.names.count
+    }
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        
+        
+        if !(cell == nil) {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = self.names[indexPath.row]
+        cell?.detailTextLabel?.text = self.contacts[indexPath.row]
+        //cell?.textLabel?.text = self.contacts[indexPath.row]
+        
+        
+        return cell!
+        
+    }
+    @objc func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected name : "+names[indexPath.row])
+    }
+    
 }
 
